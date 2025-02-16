908804a1073703a3f8dcf6e275ae39c6d28c89c9
MehemmedLinux <mehemedhumbetov1@example.com>
2025-02-16 23:10:09 +0400

    Book completed, make ImagePath column for course table

--- MiniBookShop/Models/CourseViewModel.cs
+++ MiniBookShop/Models/CourseViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace MiniBookShop
+﻿using MiniBookShop.Domain.Entities;
+
+namespace MiniBookShop
 {
     public class CourseViewModel
     {
-        public CourseViewModel()
-        {
-        }
+        public Course Course { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+        public int Price { get; set; }
+        public string Required_Skills { get; set; }
     }
 }
